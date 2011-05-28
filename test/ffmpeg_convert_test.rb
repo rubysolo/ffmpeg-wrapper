ba5d88588876324fef125606eb1b79734555e942
Solomon White <rubysolo@gmail.com>
2011-05-28 08:37:19 -0600

    fix some long lines

--- test/ffmpeg_convert_test.rb
+++ test/ffmpeg_convert_test.rb
@@ -20,27 +20,43 @@ class TestFFMPegConvert < Test::Unit::TestCase
 
   def test_filenames_with_spaces
     assert_nothing_raised {
-      FFMpeg.convert("test/fixtures/filename with spaces.mov", "test/output.flv")
+      FFMpeg.convert(
+        "test/fixtures/filename with spaces.mov",
+        "test/output.flv"
+      )
       assert File.exist?("test/output.flv")
     }
   end
 
   def test_process_output
     c = FFMpeg::Convert.new("foo", "bar")
-    c.send(:process_output_line, "FFmpeg version 0.6.2, Copyright (c) 2000-2010 the FFmpeg developers")
+    c.send(
+      :process_output_line,
+      "FFmpeg version 0.6.2, Copyright (c) 2000-2010 the FFmpeg developers"
+    )
     assert_equal "0.6.2", c.version
 
-    c.send(:process_output_line, "Input #0, mov,mp4,m4a,3gp,3g2,mj2, from 'test/input.mov':")
+    c.send(
+      :process_output_line,
+      "Input #0, mov,mp4,m4a,3gp,3g2,mj2, from 'test/input.mov':"
+    )
     assert c.capturing_input?
     refute c.capturing_output?
 
-    c.send(:process_output_line, "  Duration: 00:00:05.00, start: 0.000000, bitrate: 131 kb/s")
+    c.send(
+      :process_output_line,
+      "  Duration: 00:00:05.00, start: 0.000000, bitrate: 131 kb/s"
+    )
     assert_equal 5.0, c.total_time
 
-    c.send(:process_output_line, "    Stream #0.0(eng): Video: svq1, yuv410p, 190x240, 97 kb/s, 12 fps, 12 tbr, 600 tbn, 600 tbc")
+    str = "    Stream #0.0(eng): Video: svq1, yuv410p, 190x240, " +
+          "97 kb/s, 12 fps, 12 tbr, 600 tbn, 600 tbc"
+    c.send(:process_output_line, str)
     assert_equal 60, c.total_frames
 
-    c.send(:process_output_line, "frame=   30 fps=  0 q=2.0 Lsize=     210kB time=2.49 bitrate= 345.6kbits/s    ")
+    str = "frame=   30 fps=  0 q=2.0 Lsize=     210kB time=2.49 " +
+          "bitrate= 345.6kbits/s    "
+    c.send(:process_output_line, str)
     assert_equal 0.5, c.progress
   end
 
